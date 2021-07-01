d8e907e84e8a34fef9e0d34a5ee7095bbb8378c5
Parto Afzar <fatemehalmasian1381@gmail.com>
2021-07-01 19:37:15 +0430

    add the first part of login code.

--- src/clashroyale/controllers/LoginController.java
+++ src/clashroyale/controllers/LoginController.java
@@ -1,4 +1,22 @@
 package clashroyale.controllers;
 
+import java.awt.TextField;
+
+import clashroyale.models.LoginModel;
+import javafx.fxml.FXML;
+import jdk.internal.net.http.common.Log;
+
 public class LoginController {
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
+    private LoginModel loginModel=new LoginModel();
+
+    @FXML
+    public void loginProcess(){
+
+    }
+
+
 }
